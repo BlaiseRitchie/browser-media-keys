38decb33cb7c220908698694e61e9972de52bbfa
Blaise Ritchie <ritchie.blaise@gmail.com>
2017-09-21 10:56:16 -0700

    Added support for play.pocketcasts.com

---
+++ data/play.pocketcasts.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[@class='play_pause_button play_button']";
+MediaKeys.pauseButton = "//div[@class='play_pause_button pause_button']";
+MediaKeys.skipButton = "//div[@class='skip_forward_button']";
+MediaKeys.previousButton = "//div[@class='skip_back_button']";
